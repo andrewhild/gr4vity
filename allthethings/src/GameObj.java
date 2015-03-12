8d4fd3dc74d7e7dbd67fc32c582dc3c760dbbdf0
Andrew Hild <republiqueajamais@hotmail.com>
2015-03-12 00:53:36 -0700

    Again restructuring directories

---
+++ allthethings/src/GameObj.java
@@ -0,0 +1,16 @@
+package allthethings;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+public class GameObj {
+	
+	public GameObj() {
+		JFrame frame = new JFrame("Original and Clever Name Here");
+		frame.setVisible(true);
+
+
+	}
+
+}
